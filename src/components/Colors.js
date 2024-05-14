342d02cc96039a11cee99cbbbb761e106ef8a033
loshpek228 <sasatagaev9@gmail.com>
2024-05-14 19:00:27 +0600

    hw3

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -5,29 +5,29 @@ function Colors() {
     function handleColorChange(color) {
         setState(color)
     }
-    return (
-        <>
-            {state}
+    // return (
+    //     <>
+    //         {state}
 
-            <div style={{ display: 'flex' }}>
-                <div style={{ background: 'red', padding: '2rem' }}>
-                    <button onClick={() => handleColorChange('red')}>red</button>
-                </div>
-                <div style={{ background: 'blue', padding: '2rem' }}>
-                    <button onClick={() => handleColorChange('blue')}>blue</button>
-                </div>
-                <div style={{ background: 'yellow', padding: '2rem' }}>
+    //         <div style={{ display: 'flex' }}>
+    //             <div style={{ background: 'red', padding: '2rem' }}>
+    //                 <button onClick={() => handleColorChange('red')}>red</button>
+    //             </div>
+    //             <div style={{ background: 'blue', padding: '2rem' }}>
+    //                 <button onClick={() => handleColorChange('blue')}>blue</button>
+    //             </div>
+    //             <div style={{ background: 'yellow', padding: '2rem' }}>
 
-                    <button onClick={() => handleColorChange('yellow')}>yellow</button></div>
-                <div style={{ background: 'green', padding: '2rem' }}>
+    //                 <button onClick={() => handleColorChange('yellow')}>yellow</button></div>
+    //             <div style={{ background: 'green', padding: '2rem' }}>
 
-                    <button onClick={() => handleColorChange('green')}>green</button>
-                </div>
-            </div>
+    //                 <button onClick={() => handleColorChange('green')}>green</button>
+    //             </div>
+    //         </div>
 
-            <div style={{ background: state, padding: '2rem' }}>
-            </div>
-        </>
-    )
+    //         <div style={{ background: state, padding: '2rem' }}>
+    //         </div>
+    //     </>
+    // )
 }
 export default Colors
